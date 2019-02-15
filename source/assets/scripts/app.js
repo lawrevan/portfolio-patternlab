11607afecd579b3199f558974f4a36c6e244cc54
Larry Evans <lawrevan8484@gmail.com>
2019-02-14 19:02:00 -0600

    prismjs, syntax highlighting, js cleanup, pl config

--- source/assets/scripts/app.js
+++ source/assets/scripts/app.js
@@ -1,9 +1,7 @@
 import '../styles/app.scss';
 import './data-js';
 import './vendor/picturefill.min';
-import './fitvids';
-import './init';
-import './modernizr';
-import './nav';
-import './overlay';
-import './progress';
+// import './modernizr';
+// import './nav';
+// import './overlay';
+// import './progress';
