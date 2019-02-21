016465a088b0181e4a8134682c07ed16669d348a
Larry Evans <lawrevan8484@gmail.com>
2019-02-21 16:06:25 -0600

    include pattern_export with example, clean exports on prod build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -155,7 +155,7 @@ module.exports = env => {
           }
         }),
         ifProduction(
-          new CleanWebpackPlugin(['public'])
+          new CleanWebpackPlugin(['public','pattern_exports'])
         )
       ]),
       devServer: {
