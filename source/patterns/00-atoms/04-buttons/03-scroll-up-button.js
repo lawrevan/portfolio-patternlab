c15350af7184c589652ee1d2e9ae698687914549
Larry Evans <lawrevan8484@gmail.com>
2019-03-08 16:11:25 -0600

    scroll btns, cleanup, wip

--- source/patterns/00-atoms/04-buttons/03-scroll-up-button.js
+++ source/patterns/00-atoms/04-buttons/03-scroll-up-button.js
@@ -1,10 +1,13 @@
-// var scrollUpBtn = document.querySelector(".a-btn--scroll-up");
-//
-// if (scrollUpBtn) {
-//
-// 	scrollUpBtn.addEventListener("click", backToTop);
-//
-// 	function backToTop(){
-// 	    window.scrollTo({ top: 0, behavior: 'smooth' });
-// 	}
-// }
+// Scroll up button
+const scrollUpBtn = document.querySelector(".a-btn--scroll-up");
+const pageTop = document.querySelector(".t-homepage");
+
+if (scrollUpBtn) {
+
+	scrollUpBtn.addEventListener("click", backToTop);
+
+	function backToTop(){
+	    // window.scrollTo({ top: 0, behavior: 'smooth' });
+      zenscroll.to(pageTop, 1750);
+	}
+}
--- source/patterns/00-atoms/04-buttons/04-scroll-down-button.js
+++ source/patterns/00-atoms/04-buttons/04-scroll-down-button.js
@@ -1,15 +1,15 @@
 // Scroll down button
-// const parallax = document.querySelector('.c-parallax-bgs--top');
-// const scrollDownBtn = document.querySelector(".m-scroll-down");
-//
-// if (scrollDownBtn) {
-//   zenscroll.to(parallax, 2000);
-//
-//   // scrollDownBtn.addEventListener("click", scrollDown);
-//   //
-//   // function scrollDown() {
-//   //   parallax.scrollIntoView({
-//   //     behavior: 'smooth'
-//   //   });
-//   // }
-// }
+const parallax = document.querySelector('.c-parallax-bgs--top');
+const scrollDownBtn = document.querySelector(".a-btn--scroll-down");
+
+if (scrollDownBtn) {
+
+  scrollDownBtn.addEventListener("click", scrollDown);
+
+  function scrollDown() {
+    // parallax.scrollIntoView({
+    //   behavior: 'smooth'
+    // });
+    zenscroll.to(parallax, 1750);
+  }
+}
